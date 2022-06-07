f08f0bcfd115ac1d68e244400f6e915911492a2d
alexcoder04 <alexcoder04@protonmail.com>
2022-06-07 17:20:54 +0200

    add color generator and update colors

---
+++ _sass/colors-generate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cat <<EOF >./colors.scss
+\$color_black: $COLOR_BLACK;
+\$color_cyan: $COLOR_CYAN;
+\$color_light_grey: $COLOR_LIGHT_GREY;
+\$color_white: $COLOR_WHITE;
+\$color_yellow: $COLOR_YELLOW;
+EOF
+
